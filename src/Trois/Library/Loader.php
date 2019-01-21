bf7cdea66ffcb43949ec0c14b65ae215d6243b33
271920545@qq.com <271920545@qq.com>
2019-01-21 18:15:23 +0800

    add somethin by wandehua

---
+++ src/Trois/Library/Loader.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by WanDeHua.
+ * User: WanDeHua 
+ * Email:271920545@qq.com
+ * Date: 2019/1/21
+ * Time: 18:00
+ */
+
+namespace Trois\Library;
+
+
+class Loader {
+    public function index(){
+        echo 'hello';
+    }
+}
